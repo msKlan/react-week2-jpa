22830f57343186ca9dab739ff79b361f0ad814de
Klan <michaelsanderklan@gmail.com>
2019-11-12 10:23:04 +0100

    dev to prod i DB username

--- src/main/java/entities/RenameMe.java
+++ src/main/java/entities/RenameMe.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 
-
+//wololoololololo
 //@Entity
 //@NamedQuery(name = "RenameMe.deleteAllRows", query = "DELETE from RenameMe")
 public class RenameMe implements Serializable {
